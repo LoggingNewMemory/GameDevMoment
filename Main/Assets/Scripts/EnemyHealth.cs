7f3c90bedfb9dc814020155a5ff7b254371e03d9
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-04-09 05:46:12 +0700

    Init Add Enemy hit and death sound

--- Main/Assets/Scripts/EnemyHealth.cs
+++ Main/Assets/Scripts/EnemyHealth.cs
@@ -2,24 +2,40 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public float health = 60f; // Takes 3 shots from a 20-damage gun
+    public float health = 60f; 
+
+    [Header("Audio")]
+    public AudioClip hitSound;   // <-- NEW: Slot for the pain grunt or bullet impact
+    public AudioClip deathSound; 
 
     public void TakeDamage(float amount)
     {
         health -= amount;
         
-        // Optional: Print to console so we know it's working
         Debug.Log(gameObject.name + " took damage! Health left: " + health);
 
         if (health <= 0f)
         {
             Die();
         }
+        else
+        {
+            // If they are still alive, play the hit sound!
+            if (hitSound != null)
+            {
+                // You can add a 3rd number here for volume, like (hitSound, transform.position, 0.8f)
+                AudioSource.PlayClipAtPoint(hitSound, transform.position);
+            }
+        }
     }
 
     void Die()
     {
-        // For now, the ultimate hack: just Thanos-snap him out of existence
+        if (deathSound != null)
+        {
+            AudioSource.PlayClipAtPoint(deathSound, transform.position);
+        }
+
         Destroy(gameObject);
     }
 }
