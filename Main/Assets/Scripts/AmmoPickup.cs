232fdb6a640b17d19a37213736b20f18293e8e67
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-04-09 08:16:49 +0700

    Backpack system

--- Main/Assets/Scripts/AmmoPickup.cs
+++ Main/Assets/Scripts/AmmoPickup.cs
@@ -2,36 +2,35 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
-    [Header("Ammo Settings")]
-    public int ammoAmount = 30; // How many bullets this box gives
+    [Header("Random Ammo Settings")]
+    public int minAmmoAmount = 15; // Minimum bullets this box can give
+    public int maxAmmoAmount = 45; // Maximum bullets this box can give
 
     [Header("Audio")]
-    public AudioClip pickupSound; // Optional: The sound of grabbing ammo
+    public AudioClip pickupSound; 
 
     void OnTriggerEnter(Collider other)
     {
-        // 1. Did the player walk into this box?
         if (other.CompareTag("Player"))
         {
-            // 2. Because inactive weapons are turned OFF, this will naturally 
-            // only grab the script of the weapon you currently have in your hands!
             SimpleShoot activeGun = other.GetComponentInChildren<SimpleShoot>();
 
             if (activeGun != null)
             {
-                // 3. If our pockets are completely full, ignore the box so we don't waste it!
+                // If our pockets are already completely full, don't grab the box!
                 if (activeGun.reserveAmmo >= activeGun.maxReserveAmmo) return;
 
-                // 4. Give the gun the ammo!
-                activeGun.AddAmmo(ammoAmount);
+                // Pick a random number between Min and Max!
+                // (Note: Unity's Random.Range for integers needs +1 on the max to actually include it)
+                int randomAmmo = Random.Range(minAmmoAmount, maxAmmoAmount + 1);
 
-                // 5. Play the pickup sound
+                activeGun.AddAmmo(randomAmmo);
+
                 if (pickupSound != null)
                 {
                     AudioSource.PlayClipAtPoint(pickupSound, transform.position);
                 }
 
-                // 6. Delete the ammo box
                 Destroy(gameObject);
             }
         }
